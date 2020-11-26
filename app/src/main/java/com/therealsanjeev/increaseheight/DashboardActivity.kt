f71ead46a072b688dbaa94dd8670d6e2d70e1aa9
Sanjeev Gupta <sanjeev.mp19@nsut.ac.in>
2020-11-27 05:01:06 +0530

    nav updated

--- app/src/main/java/com/therealsanjeev/increaseheight/DashboardActivity.kt
+++ app/src/main/java/com/therealsanjeev/increaseheight/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.therealsanjeev.increaseheight
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.WindowManager
@@ -13,6 +14,7 @@ import android.widget.Toast
 
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -23,35 +25,15 @@ import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.dashboard.*
 import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration1
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     private lateinit var toggle: ActionBarDrawerToggle
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
-
-//        val toolbar: Toolbar = findViewById(R.id.toolBar)
-        setSupportActionBar(toolBar)
-//
-//        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-//        val navView: NavigationView = findViewById(R.id.nav_view)
+        setUpToolBar()
 
 
-//        appBarConfiguration= AppBarConfiguration1(setOf(
-//            R.id.nav_home,R.id.nav_notifications
-//        ),drawerLayout)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
-
-        toggle= ActionBarDrawerToggle(this,drawer_layout,R.string.open,R.string.close)
-//        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-//       nav_view
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//
-//        nav_view.setNavigationItemSelectedListener (this)
 
 //        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
@@ -68,7 +50,7 @@ class DashboardActivity : AppCompatActivity() {
         nutritionPlan.setOnClickListener {
             nutritionPlan.startAnimation(bounceAnimation)
             try {
-                val intent = Intent(this, NutritionActivity::class.java)
+                 val intent = Intent(this, NutritionActivity::class.java)
                 startActivity(intent)
             } catch (e : NullPointerException){
                 Toast.makeText(this, "Error " + e.message, Toast.LENGTH_SHORT).show()
@@ -77,6 +59,19 @@ class DashboardActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun setUpToolBar() {
+        setSupportActionBar(toolBar)
+        val actionbar=supportActionBar
+        actionbar?.title ="Increase Height!!!"
+
+        toggle= ActionBarDrawerToggle(this,drawer_layout,toolBar,R.string.open,R.string.close)
+        toggle.isDrawerIndicatorEnabled=true
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.side_menu, menu)
@@ -88,13 +83,29 @@ class DashboardActivity : AppCompatActivity() {
             return true
         when(item.itemId){
 
-            R.id.nav_home -> Toast.makeText(applicationContext,
-                "Nav Home",Toast.LENGTH_SHORT).show()
-            R.id.nav_aboutUs ->Toast.makeText(applicationContext,
-                "Nav about",Toast.LENGTH_SHORT).show()
+            R.id.side_noti_notification -> Toast.makeText(applicationContext,
+                "Notification",Toast.LENGTH_SHORT).show()
+            R.id.side_noti_setting ->Toast.makeText(applicationContext,
+                "Setting",Toast.LENGTH_SHORT).show()
         }
+
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onBackPressed() {
 
+        super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.nav_nutrition ->{
+                val intent = Intent(this, NutritionActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
