b9fd2b707716027db50912bd522f236b853440f0
Sanjeev Gupta <sanjeev.mp19@nsut.ac.in>
2021-02-19 23:55:42 +0530

    :construction: adding bottomBar

--- app/src/main/java/com/therealsanjeev/increaseheight/DashboardActivity.kt
+++ app/src/main/java/com/therealsanjeev/increaseheight/DashboardActivity.kt
@@ -2,23 +2,32 @@ package com.therealsanjeev.increaseheight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
-
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 import kotlinx.android.synthetic.main.dashboard.*
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
-class DashboardActivity : AppCompatActivity(R.layout.dashboard) {
+class DashboardActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.dashboard)
         super.onCreate(savedInstanceState)
-        navController = findNavController(R.id.main_fragment)
+//        navController = findNavController(R.id.main_fragment)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_bar)
+            .setupWithNavController(navController)
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.bottom_menu, menu)
--- app/src/main/java/com/therealsanjeev/increaseheight/fragment/DashBoard.kt
+++ app/src/main/java/com/therealsanjeev/increaseheight/fragment/DashBoard.kt
@@ -1,11 +1,15 @@
 package com.therealsanjeev.increaseheight.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.therealsanjeev.increaseheight.R
+import kotlinx.android.synthetic.main.dashboard.*
+import kotlinx.android.synthetic.main.dashboard.view.*
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class DashBoard : Fragment() {
 
@@ -16,6 +20,7 @@ class DashBoard : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view=inflater.inflate(R.layout.fragment_dashboard, container, false)
+
         return view
     }
 
